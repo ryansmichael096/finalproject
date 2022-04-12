8f0a5460d831f0952eaae0c661cb7e7b7a642fa8
ryansmichael096 <ryansmichael096@gmail.com>
2022-04-12 15:15:46 -0400

    Final project for advanced java class.

---
+++ finalproject/src/main/java/advjava_2022/finalproject/TurnSignalBlinker.java
@@ -0,0 +1,30 @@
+package advjava_2022.finalproject;
+
+import com.diozero.ws281xj.LedDriverInterface;
+import com.diozero.ws281xj.PixelAnimations;
+import com.diozero.ws281xj.rpiws281x.WS281x;
+
+public class TurnSignalBlinker {
+
+	public static void blink(String[] args) {
+		System.out.println("Test starting");
+		int brightness = 64;
+		int numPixels = 8;
+		int gpio = 18;
+		LedDriverInterface led = new WS281x(gpio, brightness, numPixels);
+		
+		for(int rep=0; rep<3; rep++) {
+			led.setPixelColourRGB(0, 255, 35, 0);
+			led.setPixelColourRGB(3, 255, 35, 0);
+			led.setPixelColourRGB(5, 255, 35, 0);
+			led.setPixelColourRGB(6, 255, 35, 0);
+			led.render();
+			System.out.println("LEDs on");
+			PixelAnimations.delay(750);
+			led.allOff();
+			System.out.println("LEDs off");
+			PixelAnimations.delay(750);
+		}
+		led.close();
+	}
+}
---
+++ finalproject/src/main/java/advjava_2022/finalproject/TurnSignalSensor.java
@@ -0,0 +1,9 @@
+package advjava_2022.finalproject;
+
+public class TurnSignalSensor {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
