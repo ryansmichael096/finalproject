b03848d346f2510676b02580f16830988a808d28
ryansmichael096 <ryansmichael096@gmail.com>
2022-04-21 13:39:10 -0400

    Update 4/21/2022 1:37PM
    Integrated first iteration of motor sensor class with blinker class.

--- finalproject/src/main/java/advjava_2022/finalproject/TurnSignalBlinker.java
+++ finalproject/src/main/java/advjava_2022/finalproject/TurnSignalBlinker.java
@@ -6,8 +6,8 @@ import com.diozero.ws281xj.rpiws281x.WS281x;
 
 public class TurnSignalBlinker {
 
-	public static void blink(String[] args) {
-		System.out.println("Test starting");
+	public void blinkLeft() {
+		System.out.println("Signaling left turn.");
 		
 		//Create variables and LedDriverInterface object.
 		int brightness = 64;
@@ -16,7 +16,32 @@ public class TurnSignalBlinker {
 		LedDriverInterface led = new WS281x(gpio, brightness, numPixels);
 		
 		//Blink LEDs 3 times with an orange color, delay of 750 milliseconds.
-		for(int rep=0; rep<3; rep++) {
+		for(int rep=0; rep<2; rep++) {
+			led.setPixelColourRGB(0, 255, 35, 0);
+			led.setPixelColourRGB(3, 255, 35, 0);
+			led.setPixelColourRGB(5, 255, 35, 0);
+			led.setPixelColourRGB(6, 255, 35, 0);
+			led.render();
+			System.out.println("LEDs on");
+			PixelAnimations.delay(750);
+			led.allOff();
+			System.out.println("LEDs off");
+			PixelAnimations.delay(750);
+		}
+		led.close();
+	}
+	
+	public void blinkRight() {
+		System.out.println("Signaling right turn.");
+		
+		//Create variables and LedDriverInterface object.
+		int brightness = 64;
+		int numPixels = 8;
+		int gpio = 18;
+		LedDriverInterface led = new WS281x(gpio, brightness, numPixels);
+		
+		//Blink LEDs 3 times with an orange color, delay of 750 milliseconds.
+		for(int rep=0; rep<2; rep++) {
 			led.setPixelColourRGB(0, 255, 35, 0);
 			led.setPixelColourRGB(3, 255, 35, 0);
 			led.setPixelColourRGB(5, 255, 35, 0);
--- finalproject/src/main/java/advjava_2022/finalproject/TurnSignalSensor.java
+++ finalproject/src/main/java/advjava_2022/finalproject/TurnSignalSensor.java
@@ -6,32 +6,23 @@ public class TurnSignalSensor {
 	
 	public static void main(String[] args) throws InterruptedException {
 		//Create object using PCA9685 class.
+		@SuppressWarnings("resource")
 		PCA9685 sensor = new PCA9685(50);
-		
-		//Assign duty cycle values to variables.
-		int c0 = sensor.getDutyUs(0);
-		int c1 = sensor.getDutyUs(1);
-		int c2 = sensor.getDutyUs(2);
-		int c3 = sensor.getDutyUs(3);
-		int c4 = sensor.getDutyUs(4);
-		int c5 = sensor.getDutyUs(5);
-		int c6 = sensor.getDutyUs(6);
-		int c7 = sensor.getDutyUs(7);
-		
-		//Display duty cycle values for channels 0 through 7.
-		while(true) {
-			System.out.println("Channel 0: " + sensor.getDutyUs(0));
-			System.out.println("Channel 1: " + sensor.getDutyUs(1));
-			System.out.println("Channel 2: " + sensor.getDutyUs(2));
-			System.out.println("Channel 3: " + sensor.getDutyUs(3));
-			System.out.println("Channel 4: " + sensor.getDutyUs(4));
-			System.out.println("Channel 5: " + sensor.getDutyUs(5));
-			System.out.println("Channel 6: " + sensor.getDutyUs(6));
-			System.out.println("Channel 7: " + sensor.getDutyUs(7));
-			System.out.println();
-			Thread.sleep(1000);
+		TurnSignalBlinker blink = new TurnSignalBlinker();
+	
+		//Call TurnSignalBlinker class if motors indicate a right or left turn.
+		while(true ) {
+			int c0 = sensor.getDutyUs(0);
+			int c1 = sensor.getDutyUs(1);
+			System.out.println("Channel 0: " + c0);
+			System.out.println("Channel 1: " + c1);
+			if (c1==7324) {
+				blink.blinkLeft();
+			}
+			else if(c0==9765) {
+				blink.blinkRight();
+			}
+			Thread.sleep(500);
 		}
-		
 	}
-
 }
